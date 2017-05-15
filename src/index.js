d1114737c0dd363ab5afa3e320025d23083a473a
Leo Creatini <creatinileandro@gmail.com>
2017-05-15 12:35:32 -0700

    Added react-web toHaveStyle matcher to test CSS rules (#11)
    * Added react-web toHaveStyle matcher to test CSS rules
    
    * Revision 1 for PR #11
    
    * Revision 2 for PR #11
    
    Updated filenames to differentiate web from native

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const toMatchStyledComponentsSnapshot = require('./matchers/toMatchStyledComponentsSnapshot')
+const toHaveStyleRule = require('./matchers/toHaveStyleRule')
 const styleSheetSerializer = require('./serializers/styleSheetSerializer')
 
 expect.addSnapshotSerializer(styleSheetSerializer)
-expect.extend({ toMatchStyledComponentsSnapshot })
+expect.extend({ toMatchStyledComponentsSnapshot, toHaveStyleRule })
