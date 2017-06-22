fdb6ace59bef16e54e96809069d4129c45ea7100
Terrence Wong <wongterrencew@gmail.com>
2017-06-22 18:42:22 +0200

    Performance improvement getClassNames (#37)
    * move getClassNames into utils
    
    Doing this because I will write a unit test for this function.
    
    There is a performance improvement that I will fix as well, the unit
    test would fail if I added it now.
    
    It would also be difficult to see the diff / performance improvement if
    I moved the code and did the fix at the same time.
    
    * improve performance for getClassNames
    
    When there are multiple children the class name is first concatted here:
    
    `classNames.concat(node.props.className.split(' '))`
    
    And because the class has been saved into `acc`, it gets concated again
    for all of its siblings here:
    
    `acc.concat(getClassNames(child, acc))`
    
    Changing to [] fixes this

--- src/utils.js
+++ src/utils.js
@@ -27,3 +27,19 @@ function getCSS(styleSheet) {
 }
 
 module.exports.getCSS = getCSS
+
+function getClassNames (node, classNames) {
+  if (node.children && node.children.reduce) {
+    classNames = node.children.reduce((acc, child) => (
+      acc.concat(getClassNames(child, []))
+    ), classNames)
+  }
+
+  if (node.props && node.props.className) {
+    return classNames.concat(node.props.className.split(' '))
+  }
+
+  return classNames
+}
+
+module.exports.getClassNames = getClassNames
