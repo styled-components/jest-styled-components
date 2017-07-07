b59790675381094126e8ffb855c2f51fec1029b5
Michele Bertoli <michele@berto.li>
2017-07-07 15:32:53 +0100

    rewrite toHaveStyleRule matcher [React]

--- src/matchers/toHaveStyleRule.js
+++ src/matchers/toHaveStyleRule.js
@@ -1,97 +1,61 @@
-/* eslint-disable one-var, no-useless-escape, no-underscore-dangle, no-nested-ternary */
-const {
-  printReceived,
-  printExpected,
-} = require('jest-matcher-utils')
+const css = require('css')
+const { printReceived, printExpected } = require('jest-matcher-utils')
 const styleSheet = require('styled-components/lib/models/StyleSheet')
 const { getCSS } = require('../utils')
 
-/**
- * Finds the generated class name from a rendered StyledComponent.
- * @param  {Object} received A rendered StyledComponent.
- * @return {String}          The generated class name.
- */
-const findClassName = (received) => {
+const getClassName = (received) => {
   let className = ''
 
-  const component = received.component || received
-
-  if (component.$$typeof === Symbol.for('react.test.json')) {
-    // react test renderer
-    className = component.props.className
-  } else if (received.node) {
-    if (received.node.$$typeof === Symbol.for('react.element')) {
-      // enzyme's shallow
-      className = received.node.props.className
-    } else {
-      // enzyme's mount
-      const renderedComponent = received.node._reactInternalInstance._renderedComponent
-
-      className = (renderedComponent._instance && renderedComponent._instance.state)
-            ? renderedComponent._instance.state.generatedClassName
-            : renderedComponent._currentElement.props.className
-    }
+  if (received.$$typeof === Symbol.for('react.test.json')) {
+    className = received.props.className
+  } else if (typeof received.prop === 'function') {
+    className = received.find('[className]').first().prop('className')
   }
-  // styled components adds the className on the end.
-  className = className.split(' ').pop()
 
-  if (received.modifier) {
-    className += received.modifier
-  }
-  return className
+  return `.${className.split(/\s/).pop()}`
 }
 
-/**
- * Tests component to see if it has the correct value for a specific CSS rule.
- * @param  {Object} received      Rendered component in unit test.
- * @param  {String} selector      The CSS rule.
- * @param  {String|RegExp} value  The CSS value.
- * @return {Object}               Object for expect to pass or not pass.
- */
-const toHaveStyleRule = (received, selector, value) => {
-  try {
-    const className = findClassName(received)
-    const css = getCSS(styleSheet)
-    const styles = new RegExp(`${className}[\\s]?{([^}]*)`, 'g').exec(css)
-    const capture = new RegExp(`${selector}:[\\s]*([^;]+)`, 'g')
+const getRules = (ast, className) => ast.stylesheet.rules.filter(
+  rule => rule.type === 'rule' && rule.selectors.includes(className)
+)
 
-    if (styles && styles[1].match(capture)) {
-      const values = styles[1].match(capture).map(r => r.replace(capture, '$1').trim())
+const getDeclarations = (rule, property) => rule.declarations.filter(
+  declaration => declaration.type === 'declaration' &&
+    declaration.property === property
+)
 
-      if (
-          values &&
-          values.some((v) => {
-            if (value instanceof RegExp) {
-              return v.match(value)
-            }
+const die = property => ({
+  pass: false,
+  message: `Property not found: ${printReceived(property)}`,
+})
 
-            return v === value
-          })
-        ) {
-        return { // Passed
-          message: () => (`
-            ${printExpected(`Expected component to have ${selector} matching ${value}`)}
-          `),
-          pass: true,
-        }
-      }
-    }
+function toHaveStyleRule(received, property, value) {
+  const className = getClassName(received)
+  const styles = getCSS(styleSheet)
+  const ast = css.parse(styles)
+  const rules = getRules(ast, className)
 
-    return { // Failed -- wrong value
-      message: () => (`
-        ${printExpected(`Expected ${className} to have ${selector} matching ${value}`)}\n
-        ${printReceived(`But received, ${styles[1] || css}`)}
-      `),
-      pass: false,
-    }
-  } catch (e) {
-    return { // Failed -- not rendered correctly
-      message: () => (`
-        ${printExpected(`Expected ${received} to be a component from react-test-renderer, or a mounted enzyme component.`)}\n
-        ${printReceived(`But had an error, ${e}`)}
-      `),
-      pass: false,
-    }
+  if (!rules.length) {
+    return die(property)
+  }
+
+  const declarations = getDeclarations(rules[0], property)
+
+  if (!declarations.length) {
+    return die(property)
+  }
+
+  const declaration = declarations[0]
+
+  const message =
+    'Expected:\n' +
+    `  ${printExpected(`${property}: ${value}`)}\n` +
+    'Received:\n' +
+    `  ${printReceived(`${property}: ${declaration.value}`)}`
+
+  return {
+    pass: value === declaration.value,
+    message,
   }
 }
 
