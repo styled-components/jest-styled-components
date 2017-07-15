619623f5a523500479aab800c41e0953156730d6
Michele Bertoli <michele@berto.li>
2017-07-15 09:32:25 +0200

    refactor all the things

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const toHaveStyleRule = require('./toHaveStyleRule')
 const styleSheetSerializer = require('./styleSheetSerializer')
-const styleSheet = require('styled-components/lib/models/StyleSheet')
-const { isOverV2, isServer } = require('./utils')
+const { resetStyleSheet } = require('./utils')
 
-if (isOverV2()) {
-  styleSheet.default.reset(isServer())
-}
+resetStyleSheet()
 
 expect.addSnapshotSerializer(styleSheetSerializer)
 expect.extend({ toHaveStyleRule })
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,15 +1,15 @@
-const { parseCSSfromHTML, getClassNames } = require('../src/utils')
+const { getStyle } = require('../src/utils')
 
-describe('parseCSSfromHTML', () => {
+describe('getStyle', () => {
   test('returns empty string when there is no style tag', () => {
-    expect(parseCSSfromHTML(null)).toBe('')
+    expect(getStyle(null)).toBe('')
   })
 
   test('returns the css from the style tag', () => {
     const css = '.a { color: red; }'
     const html = `<style>${css}</style>`
 
-    expect(parseCSSfromHTML(html)).toBe(css)
+    expect(getStyle(html)).toBe(css)
   })
 
   test('returns the css when there are multiple style tags', () => {
@@ -17,20 +17,6 @@ describe('parseCSSfromHTML', () => {
     const moreCss = '.b { color: green; }'
     const html = `<style>${css}</style><style>${moreCss}</style>`
 
-    expect(parseCSSfromHTML(html)).toBe(`${css}${moreCss}`)
-  })
-})
-
-describe('getClassNames', () => {
-  test('produces a list of unique class names from a tree', () => {
-    const tree = {
-      props: { className: 'A' },
-      children: [
-        { props: { className: 'B' } },
-        { props: { className: 'C D' } },
-      ],
-    }
-
-    expect(getClassNames(tree)).toEqual(['A', 'B', 'C', 'D'])
+    expect(getStyle(html)).toBe(`${css}${moreCss}`)
   })
 })
