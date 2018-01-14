c17a9a9a6e9217f8662bf5c33a304db0f998f7ba
Michele Bertoli <michele@berto.li>
2018-01-14 08:18:51 +0000

    support toHaveStyleRule

---
+++ test/preact/toHaveStyleRule.spec.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import render from 'preact-render-to-json'
+import styled from 'styled-components'
+import '../../src'
+
+test('basic', () => {
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `
+
+  const tree = render(<Wrapper />)
+
+  expect(tree).toHaveStyleRule('background', 'papayawhip')
+})
