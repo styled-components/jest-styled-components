563625d09fb21804ef70fddd270482bf1359b0a1
Phil Plückthun <phil@plckthn.me>
2018-01-22 19:21:10 +0000

    Upgrade to styled-components v3 and use secret internals

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,22 +1,26 @@
 const { getHashes } = require('../src/utils')
 
-jest.mock('styled-components/lib/models/StyleSheet', () => ({
-  default: {
-    instance: {
-      toHTML() {
-        return `
-          <style data-styled-components="a">
-            /* sc-component-id: sc-1 */
-            .sc-1 {}
-            .a { color: red; }
-          </style>
-          <style data-styled-components="b c">
-            /* sc-component-id: sc-2 */
-            .sc-2 {}
-            .b { color: green; }
-            .c { color: blue; }
-          </style>
-        `
+jest.mock('styled-components', () => ({
+  ServerStyleSheet: true,
+  isStyledComponent: true,
+  __DO_NOT_USE_OR_YOU_WILL_BE_HAUNTED_BY_SPOOKY_GHOSTS: {
+    StyleSheet: {
+      instance: {
+        toHTML() {
+          return `
+            <style data-styled-components="a">
+              /* sc-component-id: sc-1 */
+              .sc-1 {}
+              .a { color: red; }
+            </style>
+            <style data-styled-components="b c">
+              /* sc-component-id: sc-2 */
+              .sc-2 {}
+              .b { color: green; }
+              .c { color: blue; }
+            </style>
+          `
+        },
       },
     },
   },
