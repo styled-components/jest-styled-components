e08a27e5d1e3cd977b8e3ef4337ccfc37b6b8166
fritz-c <4413963+fritz-c@users.noreply.github.com>
2021-02-24 13:15:19 -0600

    pass indentation config to css.stringify
    Helps the library comply with Jest's serializer's indent configuration,
    which in turn fixes #355

--- src/styleSheetSerializer.js
+++ src/styleSheetSerializer.js
@@ -61,7 +61,7 @@ const getAtRules = (ast, filter) =>
       return acc.concat(atRule);
     }, []);
 
-const getStyle = classNames => {
+const getStyle = (classNames, config = {}) => {
   const ast = getCSS();
   const filter = filterRules(classNames);
   const rules = ast.stylesheet.rules.filter(filter);
@@ -69,7 +69,7 @@ const getStyle = classNames => {
 
   ast.stylesheet.rules = rules.concat(atRules);
 
-  return css.stringify(ast);
+  return css.stringify(ast, { indent: config.indent });
 };
 
 const getClassNamesFromSelectorsByHashes = (classNames, hashes) => {
@@ -123,7 +123,7 @@ module.exports = {
     classNames = filterClassNames(classNames, hashes);
     unreferencedClassNames = filterUnreferencedClassNames(unreferencedClassNames, hashes);
 
-    const style = getStyle(classNames);
+    const style = getStyle(classNames, config);
     const classNamesToReplace = getClassNamesFromSelectorsByHashes(classNames, hashes);
     const code = printer(val, config, indentation, depth, refs);
 
