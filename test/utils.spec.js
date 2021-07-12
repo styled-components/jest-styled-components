e1be59f1b2f6879b34936c0adbeec63396db9697
Aleksei Tsikov <alexey.tsikov@gmail.com>
2021-07-12 21:30:18 +0100

    Remove sc style nodes on cleanup (#382)
    * Remove sc style nodes on cleanup
    
    * Add resetStyleSheet test
    
    * Remove unneeded 'expect' import

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,9 +1,9 @@
-const {
-  __PRIVATE__: { mainSheet, masterSheet },
-} = require('styled-components');
+import styled, { __PRIVATE__ } from 'styled-components'
+import { render } from '@testing-library/react';
+import React from 'react'
+import { getHashes, resetStyleSheet } from '../src/utils';
 
-const { getHashes } = require('../src/utils');
-
+const { mainSheet, masterSheet } = __PRIVATE__
 const sheet = mainSheet || masterSheet;
 
 it('extracts hashes', () => {
@@ -36,3 +36,23 @@ it('extracts hashes', () => {
 
   expect(getHashes()).toEqual(['sc-1', 'a', 'sc-2', 'b', 'c', 'sc-3', 'd', 'e']);
 });
+
+it('resets style sheets', () => {
+  const Component = styled.div`
+    background-color: orange;
+  `
+
+  render(<Component />)
+
+  expect(
+    document.querySelectorAll('style[data-styled-version]').length,
+  ).not.toBe(0)
+  expect(sheet.names.size).not.toBe(0)
+
+  resetStyleSheet()
+
+  expect(
+    document.querySelectorAll('style[data-styled-version]').length,
+  ).toBe(0)
+  expect(sheet.names.size).toBe(0)
+})
