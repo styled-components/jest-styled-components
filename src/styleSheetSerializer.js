9ac9dcb0ca66a047d573494b1c3f14b3e8608fff
mi5q <mi5qms@gmail.com>
2024-01-29 12:50:21 +0100

    cr

--- src/styleSheetSerializer.js
+++ src/styleSheetSerializer.js
@@ -99,7 +99,7 @@ const getClassNamesFromSelectorsByHashes = (classNames, hashes) => {
 const replaceClassNames = (result, classNames, style, classNameFormatter) =>
   classNames
     .filter((className) => style.includes(className))
-    .reduce((acc, className, index) => acc.replace(new RegExp(className, 'g'), classNameFormatter(index++)), result);
+    .reduce((acc, className, index) => acc.replace(new RegExp(className, 'g'), '', result);
 
 const stripUnreferencedClassNames = (result, classNames) =>
   classNames.reduce((acc, className) => acc.replace(new RegExp(`${className}\\s?`, 'g'), ''), result);
