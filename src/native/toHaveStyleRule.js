3eea19b72ab5c351e02e37d164d24ab341a213cf
Evan Jacobs <ej@quantizor.dev>
2026-03-21 20:13:11 -0400

    fix: miscellaneous cleanup of untouched files
    - Guard expect.extend in native/index.js for non-globals mode
    - Fix missing global flag on kebab-to-camelCase regex in native matcher
      (e.g. border-top-left-radius would only convert first segment)
    - Replace O(n^2) Object.assign accumulator with Object.assign spread
    - Update FUNDING.yml to quantizor + open_collective
    - Fix duplicate test name in native spec
    - Exclude test/native from biome (uses ESM import syntax via babel)

--- src/native/toHaveStyleRule.js
+++ src/native/toHaveStyleRule.js
@@ -1,21 +1,22 @@
 const { matcherTest, buildReturnMessage } = require('../utils');
 
-function toHaveStyleRule({ props: {style} }, property, expected) {
-  const styles = Array.isArray(style) ? style.filter(x => x) : style
+function toHaveStyleRule({ props: { style } }, property, expected) {
+  const styles = Array.isArray(style) ? style.filter((x) => x) : style;
 
   /**
    * Convert style name to camel case (so we can compare)
    */
-  const camelCasedProperty = property.replace(/-(\w)/, (_, match) => match.toUpperCase());
+  const camelCasedProperty = property.replace(/-(\w)/g, (_, match) =>
+    match.toUpperCase()
+  );
 
   /**
    * Merge all styles into one final style object and search for the desired
    * stylename against this object
    */
-  const mergedStyles =
-    Array.isArray(styles)
-      ? styles.reduce((acc, item) => (Object.assign({}, acc, item)), {})
-      : styles
+  const mergedStyles = Array.isArray(styles)
+    ? Object.assign({}, ...styles)
+    : styles;
   const received = mergedStyles[camelCasedProperty];
   const pass = matcherTest(received, expected, this.isNot);
 
